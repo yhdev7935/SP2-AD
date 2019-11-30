af2a228c852cfffa4d8d7f150a307b701b3f52f5
yhdev7935 <qodydgk8624@kookmin.ac.kr>
2019-11-30 15:27:31 +0900

    Edit GameMain Layout & Improve Readability

---
+++ GameMain/GameMainButton.py
@@ -0,0 +1,46 @@
+from PyQt5.QtCore import Qt
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import *
+from options import *
+from Font import *
+from GameMainButton import *
+
+def onStartClick():
+    pass
+
+def onMapClick():
+    pass
+
+def onExitClick():
+    pass
+
+class GameMainButton(QPushButton):
+    def __init__(self, text):
+        # Initialization
+        super().__init__(text)
+        font = Font("Script MT Bold", 16)
+        font.setBold(True)
+        self.setFont(font.getFont())
+        self.setFixedSize(120, 60)
+        self.clicked.connect(self.ButtonClicked)
+
+    # Equivalent with getButton Method
+    def __str__(self):
+        return self
+
+    def ButtonClicked(self):
+        text = self.sender().text()
+
+        # When Start Button Clicked
+        if text == "Start":
+            onStartClick()
+            pass
+
+        # When Map Button Clicked
+        if text == "Button":
+            onMapClick()
+            pass
+
+        # When Exit Button Clicked
+        if text == "Exit":
+            pass
